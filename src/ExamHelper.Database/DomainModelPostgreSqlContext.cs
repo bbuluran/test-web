e13e04f800e08e844922c0cb7370af57c7918e95
bbuluran <benjie.buluran@gmail.com>
2017-08-01 14:32:39 +0800

    Add files via upload

---
+++ src/ExamHelper.Database/DomainModelPostgreSqlContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamHelper.Database
+{
+    public class DomainModelPostgreSqlContext : DbContext
+    {
+        public DomainModelPostgreSqlContext(DbContextOptions<DomainModelPostgreSqlContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+
+
+    }
+}
